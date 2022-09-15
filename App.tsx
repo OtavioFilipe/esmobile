8a5bea339c47558159e5bc27e4c2fd6cb632b76d
OtavioFilipe <darksnack16@gmail.com>
2022-09-14 23:10:08 -0300

    create components :rocket:

--- App.tsx
+++ App.tsx
@@ -1,5 +1,33 @@
-import { View } from "react-native";
+import { StatusBar } from "react-native";
+
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <View></View>;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
 }
