3fcc707f854da0dc9c0e6f867dfe7893f3adcca1
OtavioFilipe <darksnack16@gmail.com>
2022-09-14 22:14:10 -0300

    add images :rocket:

--- App.tsx
+++ App.tsx
@@ -1,33 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Button title="Send 1" />
-
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View></View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
